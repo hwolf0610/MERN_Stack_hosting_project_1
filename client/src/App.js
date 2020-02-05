12556fe7f4cf50662ff41087bae0848a911ffaef
fantasricdev <hwolf0610@hotmail.com>
2020-02-06 03:52:02 +0800

    change title

--- client/src/App.js
+++ client/src/App.js
@@ -28,24 +28,24 @@ export default class App extends Component {
   render() {
     localStorage.setItem("url", "");
     // localStorage.setItem("url", "http://localhost:3000");
-    localStorage.setItem("word1", "실적");
-    localStorage.setItem("word2", "인원수");
-    localStorage.setItem("word3", "실적 프로수");
-    localStorage.setItem("word4", "우리 총 계획");
-    localStorage.setItem("word5", "사용자관리");
-    localStorage.setItem("word6", "실적관리");
-    localStorage.setItem("word7", "계획관리");
-    localStorage.setItem("word8", "사용자추가");
-    localStorage.setItem("word9", "사용자정보변경");
-    localStorage.setItem("word10", "실적추가");
-    localStorage.setItem("word11", "실적정보변경");
-    localStorage.setItem("word12", "계획추가");
-    localStorage.setItem("word13", "계획정보변경");
-    localStorage.setItem("word14", "현재 월 실적");
-    localStorage.setItem("word15", "계획작성");
-    localStorage.setItem("word16", "주간 실적 도표");
-    localStorage.setItem("word17", "개인별 실적자료");
-    localStorage.setItem("word18", "월간 실적 도표");
+    localStorage.setItem("word1", "Quality");
+    localStorage.setItem("word2", "Members");
+    localStorage.setItem("word3", "Percent of Quality");
+    localStorage.setItem("word4", "Our all plan");
+    localStorage.setItem("word5", "User Manage");
+    localStorage.setItem("word6", "Quality Manage");
+    localStorage.setItem("word7", "Plan Manage");
+    localStorage.setItem("word8", "User add");
+    localStorage.setItem("word9", "Edit User Info");
+    localStorage.setItem("word10", "Quality add");
+    localStorage.setItem("word11", "Edit Quailty");
+    localStorage.setItem("word12", "Plan add");
+    localStorage.setItem("word13", "Edit Plan");
+    localStorage.setItem("word14", "current month quality");
+    localStorage.setItem("word15", "Plan Edit");
+    localStorage.setItem("word16", "Week Quality chart");
+    localStorage.setItem("word17", "personal quality");
+    localStorage.setItem("word18", "Month quality chart");
     return (
       <ThemeProvider theme={theme}>
         <Router history={browserHistory}>
